8cf06eee7171c2ae2e70694703207bb92fbc6745
Steven Bosnick <sbosnick@sympatico.ca>
2018-05-20 20:46:09 -0400

    Added several new tests to the testsuite.
    These tests are all designed to increase the test coverage for the
    enum_info.rs code.

---
+++ luther-derive/testsuite/fail_priority_attr_enum.rs
@@ -0,0 +1,22 @@
+// Copyright 2018 Steven Bosnick
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE-2.0 or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms
+
+#![crate_type = "lib"]
+
+extern crate luther;
+
+#[macro_use]
+extern crate luther_derive;
+
+#[derive(Lexer, Debug)]
+#[luther(priority_group = "2")]
+pub enum Token {
+    #[luther(regex = "ab")] Ab,
+    #[luther(regex = "acc*")] Acc,
+    #[luther(regex = "a(bc|de)")] Abcde(String),
+}
