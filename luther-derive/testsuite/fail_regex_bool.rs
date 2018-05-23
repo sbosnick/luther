ef342e91e54f84d1d28d5be77aaf795434d54925
Steven Bosnick <sbosnick@sympatico.ca>
2018-05-22 21:59:46 -0400

    Added new tests to the suite for enum_info coverage.
    The tests are all concerned with bad forms of the #[luther(...)]
    attribute: incorrect formulations of the attribute, unrecognied option
    names in the "..." part of the attribute, unrecognied literal forms on
    the "option = literal" part of the attribute.

---
+++ luther-derive/testsuite/fail_regex_bool.rs
@@ -0,0 +1,19 @@
+// Copyright 2018 Steven Bosnick
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE-2.0 or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms
+
+#![crate_type = "lib"]
+
+extern crate luther;
+
+#[macro_use]
+extern crate luther_derive;
+
+#[derive(Lexer, Debug)]
+pub enum Token {
+    #[luther(regex = true)] Ab,
+}
